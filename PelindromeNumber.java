6edb7bc0f8312e6a972475200545ffe2c3dc78eb
Chattanya <chattanyakumari@gmail.com>
2026-04-30 23:50:47 +0530

    Refactor isPalindrome method for clarity

--- PelindromeNumber.java
+++ PelindromeNumber.java
@@ -12,3 +12,23 @@ public boolean isPalindrome(int x) {
     }
     return true;
 }
+
+//Alternative way the optimal one
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
+//         if (x < 0 || (x % 10 == 0 && x != 0)) {
+//             return false;
+//         }
+        
+//         int reversedHalf = 0;
+//         while (x > reversedHalf) {
+//             reversedHalf = reversedHalf * 10 + x % 10;
+//             x /= 10;
+//         }
+        
+//         // For odd length numbers, ignore middle digit by comparing x with reversedHalf/10
+//         return x == reversedHalf || x == reversedHalf / 10;
+//     }
+// }
+
